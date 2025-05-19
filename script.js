640e9b47190b0884f4cd42bee51d096e4f50c765
fournest sebastien <sebfour85@gmail.com>
2025-05-19 10:12:47 +0200

    fini fini

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-// // exercice 1
+// exercice 1
 
-// let lastName = prompt(`Quel est votre nom?`);
+let lastName = prompt(`Quel est votre nom?`);
 
-// if (lastName.toLowerCase() == "Durand");
-// {
-//     console.log(`Bienvenue Mr ${lastName}`);
-// }
+if (lastName.toLowerCase() == "Durand");
+{
+    console.log(`Bienvenue Mr ${lastName}`);
+}
 
-// let firstName = prompt(`Quel est votre prénom?`);
-// let age = prompt(`Quel est votre âge?`);
-// age =parseInt(age) +1
-// if (firstName.toLowerCase() == "Michel" && age.toLowerCase() == "30");
-// {
-//     console.log(`je m'appelle ${firstName} et j'ai ${age} ans l'année prochaine`);
-// }
-// // exercice 2
-// let lenght = prompt(`Définissez une longueur`);
+let firstName = prompt(`Quel est votre prénom?`);
+let age = prompt(`Quel est votre âge?`);
+age =parseInt(age) +1
+if (firstName.toLowerCase() == "Michel" && age.toLowerCase() == "30");
+{
+    console.log(`je m'appelle ${firstName} et j'ai ${age} ans l'année prochaine`);
+}
+// exercice 2
+let lenght = prompt(`Définissez une longueur`);
 
-// let width = prompt(`Définissez une largueur`);
+let width = prompt(`Définissez une largueur`);
 
-// let surface= lenght*width;
+let surface= lenght*width;
 
-// console.log(`Le rectangle de longueur ${lenght}cm et de largeur ${width}cm a pour surface ${surface}cm2`);
-// // exercice 3
+console.log(`Le rectangle de longueur ${lenght}cm et de largeur ${width}cm a pour surface ${surface}cm2`);
+// exercice 3
 
 
-// let tempCelsius = prompt(`Quel est la temperature exterieure en °Celsius?`);
-// let tempKelvin = parseInt(tempCelsius) + 273.15;
+let tempCelsius = prompt(`Quel est la temperature exterieure en °Celsius?`);
+let tempKelvin = parseInt(tempCelsius) + 273.15;
 
-// console.log(`la temperature de ${tempCelsius} °Celsius vaut ${tempKelvin} en Kelvin`)
+console.log(`la temperature de ${tempCelsius} °Celsius vaut ${tempKelvin} en Kelvin`)
 
 // exercice 4
 
